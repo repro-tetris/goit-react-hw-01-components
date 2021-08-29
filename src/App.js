abee21848768f850f5b6553e0b43a25459943202
repro-tetris <77537560+repro-tetris@users.noreply.github.com>
2021-08-29 15:47:04 +0300

    move json to data folder

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { LessonContainer } from "./components/LessonContainer/LessonContainer";
 import { Statistics } from "./components/Statistics/Statistics";
 import { FriendList } from "./components/FriendList/FriendList";
 import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory";
-import user from "./user.json";
-import statisticalData from "./statistical-data.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
+import user from "./data/user.json";
+import statisticalData from "./data/statistical-data.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
 
 function App() {
   return (
